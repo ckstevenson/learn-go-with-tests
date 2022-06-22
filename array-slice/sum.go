f1c58bb7473284d14f15d3cad79952a9194c9699
Cameron Stevenson <cksteve@protonmail.com>
2022-06-22 23:00:29 +0200

    wip

---
+++ array-slice/sum.go
@@ -0,0 +1,25 @@
+package arrayslice
+
+func Sum(numbers []int) int {
+    sum := 0
+    for _, number := range numbers {
+        sum += number
+    }
+    return sum
+}
+
+func SumAll(numbersToSum ...[]int) ([]int) {
+    var sums []int
+    for _, numbers := range numbersToSum {
+        sums = append(sums, Sum(numbers))
+    }
+
+    //lengthOfNumbers := len(numbersToSum)
+    //sums := make([]int, lengthOfNumbers)
+    //
+    //for i, numbers := range numbersToSum {
+    //    sums[i] = Sum(numbers)
+    //}
+
+    return sums
+}
